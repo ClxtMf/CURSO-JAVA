6f9cab52152123ec78acd85a8c86dbc965facc19
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2023-06-16 19:44:54 -0300

    update

---
+++ curso_java/src/curso_java/Exercicios1/Exercicio1.java
@@ -0,0 +1,23 @@
+package curso_java.LogicaProgramacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x = 5;
+        int y = 0;
+
+        while (x > 2) {
+            System.out.print(x);
+            y = y + x;
+            x = x - 1;
+        }
+
+        // 5-0, 4-5, 3-9, 2-12
+    }
+}
