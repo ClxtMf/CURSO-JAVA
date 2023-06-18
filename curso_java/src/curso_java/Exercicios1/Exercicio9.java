a1a590a8bb78d3a0776a54f1d9f6198b58e76a6c
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2023-06-18 15:10:16 -0300

    update

---
+++ curso_java/src/curso_java/Exercicios1/Exercicio9.java
@@ -0,0 +1,40 @@
+package curso_java.Exercicios1;
+
+public class Exercicio9 {
+    class public 
+}package curso_java.Exercicios1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        int tipo = sc.nextInt();
+        while (tipo != 4) {
+            if (tipo == 1) {
+                alcool += 1;
+            } else if (tipo == 2) {
+                gasolina += 1;
+            } else if (tipo == 3) {
+                diesel += 1;
+            }
+
+            tipo = sc.nextInt();
+
+            System.out.println("MUITO OBRIGADO!");
+            System.out.println("Alcool: " + alcool);
+            System.out.println("Gasolina: " + gasolina);
+            System.out.println("Diesel: " + diesel);
+
+            sc.close();
+        }
+    }
+}
